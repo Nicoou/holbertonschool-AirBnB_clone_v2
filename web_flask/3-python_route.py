e6869abae45037f34d9b8638f93e535b125d7756
Nicoou <nf9600362@gmail.com>
2023-08-15 13:57:38 -0700

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,13 +17,14 @@ def hbnb(strict_slashes=False):
     return "HBNB"
 
 
-@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c_is_fun(text, strict_slashes=False):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
-@app.route("/python/<text>", s)
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python_is_cool(text="is cool"):
     text = text.replace("_", " ")
     return "Python {}".format(text)
