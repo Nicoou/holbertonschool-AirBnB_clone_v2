d8797f2613f70950a647ddf6e1bf924f4f55b268
Nicoou <nf9600362@gmail.com>
2023-08-15 13:42:02 -0700

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+task 0 script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello(strict_slashes=False):
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hello(strict_slashes=False):
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
